981271ec16a377a5ad4408d6a67ac5f2a929b08e
Dounia Benabdelâli <dounia.benabdelali@student.ehb.be>
2023-10-09 15:23:48 +0200

    Add project files.

---
+++ Boek.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassen_en_events
+{
+    // Enum voor Verschijningsperiode
+    public enum Verschijningsperiode
+    {
+        Dagelijks,
+        Wekelijks,
+        Maandelijks
+    }
+
+    // Basisklasse Boek
+    public class Boek
+    {
+        private string isbn;
+        private string naam;
+        private string uitgever;
+        private decimal prijs;
+
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+
+        public string Naam
+        {
+            get { return naam; }
+            set { naam = value; }
+        }
+
+        public string Uitgever
+        {
+            get { return uitgever; }
+            set { uitgever = value; }
+        }
+
+        public decimal Prijs
+        {
+            get { return prijs; }
+            set
+            {
+                // Prijs moet tussen 5€ en 50€ liggen
+                if (value >= 5 && value <= 50)
+                {
+                    prijs = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Prijs moet tussen 5€ en 50€ liggen.");
+                }
+            }
+        }
+
+        public Boek(string isbn, string naam, string uitgever, decimal prijs)
+        {
+            ISBN = isbn;
+            Naam = naam;
+            Uitgever = uitgever;
+            Prijs = prijs;
+        }
+
+        public override string ToString()
+        {
+            return $"ISBN: {ISBN}, Naam: {Naam}, Uitgever: {Uitgever}, Prijs: {Prijs:C}";
+        }
+
+        public void Lees()
+        {
+            Console.WriteLine($"Je leest het boek '{Naam}'.");
+        }
+    }
+
+    // Afgeleide klasse Tijdschrift
+    public class Tijdschrift : Boek
+    {
+        public Verschijningsperiode VerschijningsPeriode { get; set; }
+
+        public Tijdschrift(string isbn, string naam, string uitgever, decimal prijs, Verschijningsperiode periode)
+            : base(isbn, naam, uitgever, prijs)
+        {
+            VerschijningsPeriode = periode;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Verschijningsperiode: {VerschijningsPeriode}";
+        }
+    }
+
+    // Generieke klasse Bestelling<T>
+    public class Bestelling<T>
+    {
+        private static int volgnummerCounter = 1;
+
+        public int Id { get; private set; }
+        public T Item { get; private set; }
+        public DateTime Datum { get; private set; }
+        public int Aantal { get; private set; }
+        public Verschijningsperiode? Periode { get; private set; }
+
+        public Bestelling(T item, DateTime datum, int aantal, Verschijningsperiode? periode = null)
+        {
+            Id = volgnummerCounter++;
+            Item = item;
+            Datum = datum;
+            Aantal = aantal;
+            Periode = periode;
+        }
+
+        public Tuple<string, int, decimal> Bestel()
+        {
+            decimal totalePrijs = 0;
+
+            if (Item is Boek boek)
+            {
+                totalePrijs = boek.Prijs * Aantal;
+                Tuple<string, int, decimal> bestelInfo = new Tuple<string, int, decimal>(boek.ISBN, Aantal, totalePrijs);
+
+                // Trigger event
+                OnBoekBesteld($"Boek {boek.Naam} is besteld. ISBN: {boek.ISBN}, Aantal: {Aantal}, Totale Prijs: {totalePrijs:C}");
+
+                return bestelInfo;
+            }
+            else if (Item is Tijdschrift tijdschrift)
+            {
+                totalePrijs = tijdschrift.Prijs * Aantal;
+                Console.WriteLine($"Tijdschrift {tijdschrift.Naam} is besteld. Aantal: {Aantal}, Totale Prijs: {totalePrijs:C}");
+                return null; // Tuple is niet van toepassing op tijdschriften
+            }
+
+            //Console.WriteLine($"Bestelling {Id}: {Aantal} exemplaren van {Item.ToString()} voor een totaalbedrag van {totalePrijs:C}.");
+            // return new Tuple<string, int, decimal>(Id.ToString(), Aantal, totalePrijs
+            return null;
+        }
+
+        public static event EventHandler<string> BoekBesteld;
+
+        public static void OnBoekBesteld(string boodschap)
+        {
+            BoekBesteld?.Invoke(null, boodschap);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+
+namespace Klassen_en_events
+{
+    internal class Program
+    {
+        static List<Boek> boeken = new List<Boek>();
+        static List<Tijdschrift> tijdschriften = new List<Tijdschrift>();
+        static List<Bestelling<object>> bestellingen = new List<Bestelling<object>>();
+        //static List<Boek> boeken
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Voeg een nieuw boek toe");
+                Console.WriteLine("2. Voeg een nieuw tijdschrift toe");
+                Console.WriteLine("3. Plaats een bestelling");
+                Console.WriteLine("4. Toon bestellingen");
+                Console.WriteLine("5. Stoppen");
+
+                Console.Write("Selecteer een optie (1-5): ");
+                string keuze = Console.ReadLine();
+
+                switch (keuze)
+                {
+                    case "1":
+                        VoegBoekToe();
+                        break;
+                    case "2":
+                        VoegTijdschriftToe();
+                        break;
+                    case "3":
+                        PlaatsBestelling();
+                        break;
+                    case "4":
+                        ToonBestellingen();
+                        break;
+                    case "5":
+                        return;
+                    default:
+                        Console.WriteLine("Ongeldige keuze. Probeer opnieuw.");
+                        break;
+                }
+            }
+        }
+
+        static void VoegBoekToe()
+        {
+            Console.Write("Voer ISBN in: ");
+            string isbn = Console.ReadLine();
+
+            Console.Write("Voer boeknaam in: ");
+            string naam = Console.ReadLine();
+
+            Console.Write("Voer uitgever in: ");
+            string uitgever = Console.ReadLine();
+
+            Console.Write("Voer prijs in: ");
+            decimal prijs = decimal.Parse(Console.ReadLine());
+
+            Boek boek = new Boek(isbn, naam, uitgever, prijs);
+            boeken.Add(boek);
+
+            Console.WriteLine("Boek is toegevoegd.");
+        }
+
+        static void VoegTijdschriftToe()
+        {
+            Console.Write("Voer ISBN in: ");
+            string isbn = Console.ReadLine();
+
+            Console.Write("Voer tijdschriftnaam in: ");
+            string naam = Console.ReadLine();
+
+            Console.Write("Voer uitgever in: ");
+            string uitgever = Console.ReadLine();
+
+            Console.Write("Voer prijs in: ");
+            decimal prijs = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Voer verschijningsperiode in (Dagelijks, Wekelijks, Maandelijks): ");
+            Verschijningsperiode periode = (Verschijningsperiode)Enum.Parse(typeof(Verschijningsperiode), Console.ReadLine(), true);
+
+            Tijdschrift tijdschrift = new Tijdschrift(isbn, naam, uitgever, prijs, periode);
+            tijdschriften.Add(tijdschrift);
+
+            Console.WriteLine("Tijdschrift is toegevoegd.");
+        }
+
+        static void PlaatsBestelling()
+        {
+            Console.Write("Selecteer een item (Boek of Tijdschrift): ");
+            string itemType = Console.ReadLine();
+
+            if (itemType.Equals("Boek", StringComparison.OrdinalIgnoreCase))
+            {
+                ToonBeschikbareBoeken();
+            }
+            else if (itemType.Equals("Tijdschrift", StringComparison.OrdinalIgnoreCase))
+            {
+                ToonBeschikbareTijdschriften();
+            }
+            else
+            {
+                Console.WriteLine("Ongeldig itemtype. Probeer opnieuw.");
+                return;
+            }
+
+            Console.Write("Voer het nummer van het geselecteerde item in: ");
+            int itemIndex = int.Parse(Console.ReadLine());
+
+            if (itemType.Equals("Boek", StringComparison.OrdinalIgnoreCase) && itemIndex >= 0 && itemIndex < boeken.Count)
+            {
+                Boek geselecteerdBoek = boeken[itemIndex];
+                VoegBestellingToe(geselecteerdBoek);
+            }
+            else if (itemType.Equals("Tijdschrift", StringComparison.OrdinalIgnoreCase) && itemIndex >= 0 && itemIndex < tijdschriften.Count)
+            {
+                Tijdschrift geselecteerdTijdschrift = tijdschriften[itemIndex];
+                VoegBestellingToe(geselecteerdTijdschrift);
+            }
+            else
+            {
+                Console.WriteLine("Ongeldige selectie. Probeer opnieuw.");
+            }
+        }
+
+        static void VoegBestellingToe(object item)
+        {
+            Console.Write("Voer besteldatum in (dd/mm/jjjj): ");
+            DateTime datum = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+
+            Console.Write("Voer aantal in: ");
+            int aantal = int.Parse(Console.ReadLine());
+
+            if (item is Boek boek)
+            {
+                Bestelling<object> bestelling = new Bestelling<object>(boek, datum, aantal);
+                bestellingen.Add(bestelling);
+                Console.WriteLine($"Boek \"{boek.Naam}\" is besteld.");
+            }
+            else if (item is Tijdschrift tijdschrift)
+            {
+                Console.Write("Voer verschijningsperiode in (Dagelijks, Wekelijks, Maandelijks): ");
+                Verschijningsperiode periode = (Verschijningsperiode)Enum.Parse(typeof(Verschijningsperiode), Console.ReadLine(), true);
+
+                Bestelling<object> bestelling = new Bestelling<object>(tijdschrift, datum, aantal, periode);
+                bestellingen.Add(bestelling);
+                Console.WriteLine($"Tijdschrift \"{tijdschrift.Naam}\" is besteld.");
+            }
+        }
+
+        static void ToonBeschikbareBoeken()
+        {
+            Console.WriteLine("Beschikbare boeken:");
+            for (int i = 0; i < boeken.Count; i++)
+            {
+                Console.WriteLine($"{i}. {boeken[i].Naam}");
+            }
+        }
+
+        static void ToonBeschikbareTijdschriften()
+        {
+            Console.WriteLine("Beschikbare tijdschriften:");
+            for (int i = 0; i < tijdschriften.Count; i++)
+            {
+                Console.WriteLine($"{i}. {tijdschriften[i].Naam}");
+            }
+        }
+
+        static void ToonBestellingen()
+        {
+            Console.WriteLine("Geplaatste bestellingen:");
+            foreach (var bestelling in bestellingen)
+            {
+                Console.WriteLine($"ID: {bestelling.Id}, Item: {bestelling.Item.ToString()}, Datum: {bestelling.Datum.ToShortDateString()}, Aantal: {bestelling.Aantal}");
+            }
+
+        }
+
+    }
+}
